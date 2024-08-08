3705d83755da1258a2f5259b0c33664e41a91c92
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-08-08 13:41:02 +0545

    fix: conformation email

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
+from .views import RegisterView, LoginView, LogoutView,confirm_registration
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path("confirm/<str:uidb64>/<str:token>/",confirm_registration,name="confirm_registration"),
 ]
