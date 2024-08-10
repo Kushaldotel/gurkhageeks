3a1d7d3e694d60b04abc6c28822296ced8b9b520
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-08-10 23:10:23 +0545

    feat: mostliked post and latest post

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import PostsViewset,CategoriesView,Recentpostsview, Postcomment,PostLikeDislikeView
+from .views import PostsViewset,CategoriesView,Recentpostsview, Postcomment,PostLikeDislikeView,Mostlikedpost,Latestpostview
 from rest_framework.routers import DefaultRouter
 router= DefaultRouter()
 router.register('', PostsViewset)
@@ -10,5 +10,7 @@ urlpatterns = [
     path('recentposts/',Recentpostsview.as_view(), name='recentposts'),
     path('postcomment/<int:pk>/',Postcomment.as_view(), name='postcomment'),
     path("postlike/<int:pk>/",PostLikeDislikeView.as_view(), name="postlikedislike"),
+    path("mostlikedpost/",Mostlikedpost.as_view(), name="mostlikedpost"),
+    path("latestposts/",Latestpostview.as_view(),name="latestposts"),
     path('', include(router.urls)),
 ]
