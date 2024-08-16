cc52d7ef43ca40e5ec83719afb4733c1bc81fac7
Pawan Sapkota <pawansapkota100@gmail.com>
2024-08-16 15:34:33 +0545

    feat: image field in project_showcase feature

--- projectshowcase/admin.py
+++ projectshowcase/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import projectshowcase
+from .models import projectshowcase,ProjectShowcaseImage
 
 
 admin.site.register(projectshowcase)
+admin.site.register(ProjectShowcaseImage)
--- projectshowcase/views.py
+++ projectshowcase/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import ProjectShowcaseSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import projectshowcase
+from rest_framework.parsers import MultiPartParser
 
 
 # Create your views here.
@@ -11,6 +12,7 @@ class ProjectShowcaseView(ModelViewSet):
     queryset= projectshowcase.objects.all()
     serializer_class = ProjectShowcaseSerializer
     http_method_names = ['get', 'post', 'patch', 'delete']
+    parser_classes= [MultiPartParser]
 
     def get_permissions(self):
         if self.request.method in ['GET']:
