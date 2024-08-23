a8c02ffed8d24cdd2256adc3897975823bad6857
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-08-24 00:35:49 +0545

    feat: Softdelete model

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,22 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    objects = SoftDeleteManager()
+
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.save()
+    
+    def permanent_delete(self, *args, **kwargs):
+        super().delete()
+
+    class Meta:
+        abstract = True
+
+
