8d9b4073662fcfcb98e7e18ec0618957f60e1bcf
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-08-27 14:05:08 +0545

    fix: added url for individual blog post and project showcase

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from core.models import Post
 from projectshowcase.models import projectshowcase
+from rest_framework.reverse import reverse
 User=get_user_model()
 
 
@@ -19,13 +20,38 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return Post.objects.filter(author=obj).count()
 
     def get_blogs(self, obj):
-        return Post.objects.filter(author=obj ).values('id', 'title', 'thumbnail', 'created_at')
+        request = self.context.get('request')  # Get the request object from the context
+        blogs = Post.objects.filter(author=obj).values('id', 'title', 'thumbnail', 'created_at')
+        # Construct full URLs for each blog
+        blog_list = [
+            {
+                'id': blog['id'],
+                'title': blog['title'],
+                'thumbnail': blog['thumbnail'],
+                'created_at': blog['created_at'],
+                'url': request.build_absolute_uri(reverse('post-detail', kwargs={'pk': blog['id']}))  # Adjust 'post-detail' to your actual URL name
+            }
+            for blog in blogs
+        ]
+        return blog_list
 
     def get_total_project_showcase(self, obj):
         return projectshowcase.objects.filter(user=obj ).count()
 
     def get_project_showcase(self, obj):
-        return projectshowcase.objects.filter(user=obj ).values('id', 'name','created_at')
+        request = self.context.get('request')  # Get the request object from the context
+        project_showcases = projectshowcase.objects.filter(user=obj).values('id', 'name', 'created_at')
+        # Construct full URLs for each project showcase
+        project_showcase_list = [
+            {
+                'id': showcase['id'],
+                'name': showcase['name'],
+                'created_at': showcase['created_at'],
+                'url': request.build_absolute_uri(reverse('projectshowcase-detail', kwargs={'pk': showcase['id']}))  # Adjust 'projectshowcase-detail' to your actual URL name
+            }
+            for showcase in project_showcases
+        ]
+        return project_showcase_list
 
 class UserUpdateSerailizer(serializers.ModelSerializer):
     
