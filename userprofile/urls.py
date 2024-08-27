edab5595bb011f7f19b840497d514b13851e68f0
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-08-27 13:42:21 +0545

    feat: user profile get, update

---
+++ userprofile/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserProfileView
+
+router= DefaultRouter()
+# router.register(r'userprofile', UserProfileView, basename='userprofile')
+
+
+urlpatterns = [
+    # path('', include(router.urls)),  # Include the router's URLs
+    path('userprofile/', UserProfileView.as_view({'get': 'list', 'patch': 'partial_update'}), name='userprofile'),
+]
---
+++ userprofile/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserProfileSerializer,UserUpdateSerailizer
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+User= get_user_model()
+# Create your views here.
+
+
+class UserProfileView(ModelViewSet):
+    parser_classes= [MultiPartParser]
+    http_method_names = ['get', 'patch']
+
+    def get_queryset(self):
+        # Filter queryset to only include the currently authenticated user
+        user_id = self.request.user.id
+        return User.objects.filter(id=user_id)
+
+    def get_serializer_class(self):
+        # Return different serializers based on HTTP method
+        if self.request.method == 'GET':
+            return UserProfileSerializer
+        return UserUpdateSerailizer
+
+    def update(self, request, *args, **kwargs):
+
+        user = self.request.user
+
+        serializer = UserUpdateSerailizer(instance=user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
