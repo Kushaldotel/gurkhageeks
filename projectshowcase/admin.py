b7f6c45148e29574cc4eaa2f4faf8934857506e2
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-08-31 18:18:04 +0545

    fix: admin dashboard display of projectshowcase and contacts app

--- projectshowcase/admin.py
+++ projectshowcase/admin.py
@@ -3,6 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import projectshowcase,ProjectShowcaseImage
 
+class ProjectShowcaseAdmin(admin.ModelAdmin):
+    list_display=['user','name','type_of_project','technology_used']
+    ordering=['-created_at']
+    fieldsets=[
+        (
+            "Project Imformation",
+            {
+                'fields':['name','type_of_project','description_of_project','Challenges_faced','suggestion','technology_used','project_link','user']
+            }
+        ),
+        
+        (
+            'timestamp',
+            {
+                "fields":["created_at"],
+                "classes":('collapse',)
+            }
+        )
+    ]
+    readonly_fields=['created_at']
 
-admin.site.register(projectshowcase)
+
+
+admin.site.register(projectshowcase,ProjectShowcaseAdmin)
 admin.site.register(ProjectShowcaseImage)
