9b00d57724b64a1f7ba56a130cb42db1fc13def7
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-09-06 10:04:57 +0545

    feat: forgot password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 # urls.py
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView,confirm_registration,ProfileView,TermsandServicesView
+from .views import RegisterView, LoginView, LogoutView,confirm_registration,ProfileView,TermsandServicesView, ForgotPassword, ResetPasswordView
 
 urlpatterns = [
     path('register/', RegisterView.as_view({'post': 'create'}), name='register'),
@@ -8,6 +8,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('terms-services/', TermsandServicesView.as_view(), name='terms'),
     path("confirm/<str:uidb64>/<str:token>/",confirm_registration,name="confirm_registration"),
-    path("profile/",ProfileView.as_view(),name="profile")
+    path("profile/",ProfileView.as_view(),name="profile"),
+    path('forgot-password',ForgotPassword.as_view({'post': 'create'}),name='forgot_password'),
+    path('reset-password/<str:uidb64>/<str:token>/',ResetPasswordView.as_view({'post': 'create'}),name='reset_password'),
 
 ]
