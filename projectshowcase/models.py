74bd41c731b5c6be1a2ecbb45067c07a59ad8b2e
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-09-09 18:21:59 +0545

    feat: implement slug field in projectshowcase

--- projectshowcase/models.py
+++ projectshowcase/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 user= get_user_model()
 
 # Create your models here.
 
 class projectshowcase(models.Model):
     name=models.CharField(max_length=100, verbose_name="name of project")
+    slug= models.SlugField(max_length=100, unique=True, null=True, blank=True)
     type_of_project= models.CharField(max_length=100, verbose_name="Type of Project eg.mobileapp, ML")
     description_of_project= models.TextField()
     Challenges_faced= models.TextField()
@@ -17,6 +19,10 @@ class projectshowcase(models.Model):
 
     def __str__(self):
         return self.name
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug= slugify(self.name)
+        super(projectshowcase,self).save(*args, **kwargs)
 
 class ProjectShowcaseImage(models.Model):
     project_showcase= models.ForeignKey(projectshowcase, on_delete=models.CASCADE, related_name="images")
