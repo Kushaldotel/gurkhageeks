848567a59b5d04e62d91c333b791bfade6af4802
Pawan Sapkota <pawansapkota100@gmail.com>
2024-09-13 11:05:33 +0545

    feat: comment system

--- core/models.py
+++ core/models.py
@@ -32,24 +32,35 @@ class Post(models.Model):
         super(Post, self).save(*args, **kwargs)
 
 
-
-class PostComments(models.Model):
+class Comment(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
     post= models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment=models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    content= models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,related_name='replies')
+    created_at= models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.comment} by {self.author}"
+        return self.content
 
-class CommentReply(models.Model):
-    comment= models.ForeignKey(PostComments, on_delete=models.CASCADE, related_name='replies')
-    reply= models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.reply} by {self.author}"
+# class PostComments(models.Model):
+#     post= models.ForeignKey(Post, on_delete=models.CASCADE)
+#     comment=models.TextField()
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,related_name='replies')
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.comment} by {self.author}"
+
+# class CommentReply(models.Model):
+#     comment= models.ForeignKey(PostComments, on_delete=models.CASCADE, related_name='replies')
+#     reply= models.TextField()
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.reply} by {self.author}"
 
 class Postinteraction(models.Model):
     post= models.ForeignKey(Post, on_delete=models.CASCADE)
