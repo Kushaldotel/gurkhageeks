bead2b4b320042fc70210fcdda1ec9df3b903085
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-09-17 16:23:48 +0545

    testing

--- authentication/urls.py
+++ authentication/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("confirm/<str:uidb64>/<str:token>/",confirm_registration,name="confirm_registration"),
     path("profile/",ProfileView.as_view(),name="profile"),
     path('forgot-password',ForgotPassword.as_view({'post': 'create'}),name='forgot_password'),
+    
     path('reset-password/<str:uidb64>/<str:token>/',ResetPasswordView.as_view({'post': 'create'}),name='reset_password'),
 
 ]
