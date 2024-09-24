df657c8c768bba5f2f7bedc378e094b435764a93
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-09-24 19:08:41 +0545

    Feat/sociallogin (#6)
    * feat: google signin
    
    * fix: code
    
    * fix: requirement.txt
    
    ---------
    
    Co-authored-by: pawan sapkota <pawansapkota@pawans-MacBook-Air.local>

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 # urls.py
-from django.urls import path
+from django.urls import path,include
 from .views import (RegisterView, LoginView, LogoutView,confirm_registration,
                     ProfileView,TermsandServicesView, ForgotPassword, ResetPasswordView,OrganisationRegistrationView)
 
@@ -12,7 +12,7 @@ urlpatterns = [
     path("confirm/<str:uidb64>/<str:token>/",confirm_registration,name="confirm_registration"),
     path("profile/",ProfileView.as_view(),name="profile"),
     path('forgot-password',ForgotPassword.as_view({'post': 'create'}),name='forgot_password'),
-    
     path('reset-password/<str:uidb64>/<str:token>/',ResetPasswordView.as_view({'post': 'create'}),name='reset_password'),
+    path('', include('sociallogin.urls'))
 
 ]
