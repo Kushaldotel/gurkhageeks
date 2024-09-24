133eb17f81ba69fdd794c957ac4472e21b55adcb
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-09-24 19:01:48 +0545

    fix: code

--- sociallogin/views.py
+++ sociallogin/views.py
@@ -3,17 +3,19 @@ from rest_framework import generics
 from .serializers import GoogleLoginSerializer
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+import requests
 
 # Create your views here.
 class GoogleLoginApiView(generics.GenericAPIView):
     serializer_class= GoogleLoginSerializer
 
     def post(self, request):
-        token= self.request.data('token')
+        token= self.request.data.get('token')
         serializer= self.get_serializer(data= request.data)
         if serializer.is_valid(raise_exception= True):
             token= serializer.validated_data['token']
-            response= request.get(f'https://oauth2.googleapis.com/tokeninfo?id_token={token}')
+            response = requests.get(f'https://oauth2.googleapis.com/tokeninfo?id_token={token}')
+
             
             if response.status_code != 200:
                 return Response({'message': 'Invalid token'}, status= 400)
